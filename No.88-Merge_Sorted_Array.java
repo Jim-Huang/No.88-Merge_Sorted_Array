81683a29b6e32b7ff1c0867a7d9d5b6d084b4d08
Jim-Huang <gh555236@gmail.com>
2020-01-21 23:50:02 +0800

    Create No.88-Merge_Sorted_Array.java

---
+++ No.88-Merge_Sorted_Array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index = (m+n)-1;
+        m--;
+        n--;
+        while(m>=0 && n>=0){
+            nums1[index] = nums1[m]>nums2[n]?nums1[m]:nums2[n];
+            if(nums1[m]>nums2[n]){
+                nums1[index] = nums1[m];
+                m--;
+            }
+            else{
+                nums1[index] = nums2[n];
+                n--;
+            }
+            index--;
+        }
+        while(n>=0){
+            nums1[index] = nums2[n];
+            index--;
+            n--;
+        }
+        
+    }
+}
